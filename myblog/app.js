a011f2216aadae8e11a3f38371e927ae16ad96b7
Kyle <nonplayercontrolled@gmail.com>
2024-02-05 23:28:10 -0500

    big changes

--- myblog/app.js
+++ myblog/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var ctrlBlog = require('./controllers/blog');
+var home = require('./controllers/home');
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/add', ctrlBlog.add);
+app.use('/list', ctrlBlog.list);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myblog/routes/index.js
+++ myblog/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Kyle Kalbach\'s Blog Site' });
 });
 
 module.exports = router;
